afc63a249d885e7ad149e9700e2db6463a300c6c
victor <victorh1705@gmail.com>
2021-04-21 21:35:47 -0300

    adding fonts

--- meal_app/meal_app/lib/categories_screen.dart
+++ meal_app/meal_app/lib/categories_screen.dart
@@ -19,7 +19,7 @@ class CategoriesScreen extends StatelessWidget {
           maxCrossAxisExtent: 200,
           childAspectRatio: 3 / 2,
           crossAxisSpacing: 20,
-          mainAxisExtent: 20,
+          mainAxisSpacing: 20,
         ),
       ),
     );
